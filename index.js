262e561472e8fe111427e42d08af32a27c5173f3
werter <werter@hotmail.com.br>
2020-12-03 19:53:07 -0300

    Manual Merge na index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 var express = require('express')
 
 const app = express();
 app.listen(4000)
-=======
-novo index deve ser implementado aqui
->>>>>>> 388d374c473aa1aef21ff51954c7bbb744240d1a
