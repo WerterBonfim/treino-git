b62eaec5287930c4e3c1ef43f5d487184b8b6433
Werter Bonfim <werter@hotmail.com.br>
2020-12-07 16:04:30 -0300

    Testando Atom

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 var express = require('express')
 
 const app = express();
 app.listen(4000)
-=======
-novo index deve ser implementado aqui
->>>>>>> 388d374c473aa1aef21ff51954c7bbb744240d1a
+
+const teste = () => {
+  console.log('teste')
+}
